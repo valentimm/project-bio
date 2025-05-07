555128b2a58f33a4227cb69a34934bc83f3418f7
Matheus Valentim <meduardo.valentim@gmail.com>
2025-05-06 22:41:34 -0300

    feat: create base layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,24 @@
 import "./globals.css";
 
+import { Red_Hat_Display } from 'next/font/google'
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"]
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
-        className={`antialiased`}>{children}</body>
+        className={`${redHatDisplay.className} bg-background-primary text-content-body antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
