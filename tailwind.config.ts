02e01470883cdaa20167fbe1111860f99b4950c9
Matheus Valentim <meduardo.valentim@gmail.com>
2025-05-12 00:08:53 -0300

    fix: correct Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,22 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        "background-primary": "#050505",
-        "background-secondary": "#0F0F10",
-        "background-tertiary": "#19191A",
-        "content-body": "#CDCBCC",
-        "content-placeholder": "#827D7F",
-        "content-headline": "#B2B2B2",
-        "border-primary": "#19191A",
-        "border-secondary": "#323234",
-        "border-tertiary": "#97979B",
-        "accent-purple": "#4B2DBB",
-        "accent-green": "#87BB2D",
-        "accent-pink": "#B5446B"
+
       },
     },
-  },
-  plugins: [],
-};
+  }
+
+
 export default config;
