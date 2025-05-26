469de639216fde748183cfc7d0d92f7602d1d963
Matheus Valentim <meduardo.valentim@gmail.com>
2025-05-26 13:18:10 -0300

    feat: implement auth

--- app/components/landing-page/header.tsx
+++ app/components/landing-page/header.tsx
@@ -1,6 +1,10 @@
+import { auth } from "@/app/lib/auth";
 import Button from "../ui/button";
+import { handleAuth } from "@/app/actions/manage-auth";
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
+
   return (
     <div className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
       <div className="flex items-center gap-4">
@@ -8,8 +12,10 @@ export default function Header() {
         <h3 className="text-white text-2xl font-bold">ProjectInBio</h3>
       </div>
       <div className="flex items-center gap-4">
-        <Button className="cursor-pointer">Minha Página</Button>
-        <Button className="cursor-pointer">Sair</Button>
+        {session && <Button>Minha Página</Button>}
+        <form action={handleAuth}>
+          <Button>{session ? "Sair" : "Login"}</Button>
+        </form>
       </div>
     </div>
   );
