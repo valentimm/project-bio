9d4c27b57e277ad55a2eb34479a16d6460c7d184
Matheus Valentim <meduardo.valentim@gmail.com>
2025-05-27 23:00:54 -0300

    feat: create new project component

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -1,8 +1,11 @@
 import ProjectCard from "@/app/components/commons/project-card";
 import TotalVisits from "@/app/components/commons/total-visits";
 import UserCard from "@/app/components/commons/user-card";
-import { Plus } from "lucide-react";
+import { auth } from "@/app/lib/auth";
+import { getProfileData } from "@/app/server/get-profile-data";
 import Link from "next/link";
+import { notFound } from "next/navigation";
+import NewProject from "./new-project";
 
 export default async function ProfilePage({
   params,
@@ -10,6 +13,20 @@ export default async function ProfilePage({
   params: Promise<{ profileId: string }>;
 }) {
   const { profileId } = await params;
+
+  const profileData = await getProfileData(profileId);
+
+  if (!profileData) return notFound();
+
+  //TODO: GET projects
+
+  const session = await auth();
+
+  const isOwner = profileData.userId === session?.user?.id;
+
+  //TODO> Add page view
+
+  //TODO: If owner not on trial, redirect to upgrade page
 
   return (
     <div className="relative h-screen flex p-20 overflow-hidden">
@@ -32,10 +49,11 @@ export default async function ProfilePage({
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
-        <button className="w-[340px] h-[132px] rounded-[20px] bg-background-secondary flex items-center gap-2 justify-center hover:border hover:border-dashed border-border-secondary">
-          <Plus className="size-10 text-accent-green" />
-          <span>Novo projeto</span>
-        </button>
+        {
+          isOwner && (
+            <NewProject profileId={profileId} />
+          )
+        }
       </div>
       <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
         <TotalVisits />
